c3fb95802aa4e14f3a918c7cd4bdef2f86af542e
Toby Schachman <tobyschachman@gmail.com>
2011-02-09 21:46:18 -0500

    made x1, x2, t1, t2 range from 0 to 1 (instead of -1 to 1)

--- src/testApp.cpp
+++ src/testApp.cpp
@@ -4,13 +4,13 @@
 void testApp::setup(){
 	ofBackground(255,255,255);
 	
-	x1 = -1;
-	t1 = 0;
+	x1 = 0;
+	t1 = 0.5;
 	x2 = 1;
-	t2 = 0;
+	t2 = 0.5;
 	
 	
-	increment = 0.05;
+	increment = 0.025;
 
 	movie.loadMovie("movies/MVI_1150.AVI");
 	width = movie.getWidth();
@@ -48,7 +48,7 @@ void testApp::draw(){
 	
 	// Draw X-Time Graph
 	ofRect(0, 0, 200, 200);	
-	ofLine((x1+1)*100, (-t1+1)*100, (x2+1)*100, (-t2+1)*100);
+	ofLine(x1*200, (1-t1)*200, x2*200, (1-t2)*200);
 	
 	// Draw Instructions
 	ofDrawBitmapString("UP: zoom in\nDOWN: zoom out\nLEFT: pan left\nRIGHT: pan right\nZ: rotate left\nX: rotate right", 20, 220);
@@ -60,8 +60,8 @@ void testApp::draw(){
 	unsigned char dest[(int) width * (int) height * 3]; // destination image
 	
 	for (int destx = 0; destx < width; destx++) {
-		int srcx = ofMap(destx, 0, width, (x1+1)*(width-1)/2, (x2+1)*(width-1)/2, true);
-		int srcframe = ofMap(destx, 0, width, (t1+1)*(frames-1)/2, (t2+1)*(frames-1)/2, true);
+		int srcx = ofMap(destx, 0, width, x1*(width-1), x2*(width-1), true);
+		int srcframe = ofMap(destx, 0, width, t1*(frames-1), t2*(frames-1), true);
 		for (int y = 0; y < height; y++) {
 			for (int color = 0; color < 3; color++) {
 				dest[(y * (int) width + destx) * 3 + color] = wholeMovie[(srcframe * (int) width * (int) height + y * (int) width + srcx) * 3 + color];
@@ -81,55 +81,55 @@ void testApp::keyPressed  (int key){
 	switch (key) {
 		case OF_KEY_UP:
 			// zoom in
-			if (x1 == -1 && x2 == 1) {
-				t1 = ofClamp(t1 + increment, -1, 1);
-				t2 = ofClamp(t2 - increment, -1, 1);
-			} else if (t1 == -1 && t2 == 1) {
-				x1 = ofClamp(x1 - increment, -1, 1);
-				x2 = ofClamp(x2 + increment, -1, 1);
+			if (x1 == 0 && x2 == 1) {
+				t1 = ofClamp(t1 + increment, 0, 1);
+				t2 = ofClamp(t2 - increment, 0, 1);
+			} else if (t1 == 0 && t2 == 1) {
+				x1 = ofClamp(x1 - increment, 0, 1);
+				x2 = ofClamp(x2 + increment, 0, 1);
 			}
 			break;
 		case OF_KEY_DOWN:
 			// zoom out
-			if (t1 == -1 && t2 == 1) {
-				x1 = ofClamp(x1 + increment, -1, 1);
-				x2 = ofClamp(x2 - increment, -1, 1);
+			if (t1 == 0 && t2 == 1) {
+				x1 = ofClamp(x1 + increment, 0, 1);
+				x2 = ofClamp(x2 - increment, 0, 1);
 				if (x1 > x2) {
 					float middle = (x1 + x2) / 2;
 					x1 = middle;
 					x2 = middle;
 				}
-			} else if (x1 == -1 && x2 == 1) {
-				t1 = ofClamp(t1 - increment, -1, 1);
-				t2 = ofClamp(t2 + increment, -1, 1);
+			} else if (x1 == 0 && x2 == 1) {
+				t1 = ofClamp(t1 - increment, 0, 1);
+				t2 = ofClamp(t2 + increment, 0, 1);
 			}
 			break;
 		case OF_KEY_LEFT:
 			// pan left
-			if (x1 == -1 && x2 == 1) {
-				t1 = ofClamp(t1 - increment, -1, 1);
-				t2 = ofClamp(t2 - increment, -1, 1);
+			if (x1 == 0 && x2 == 1) {
+				t1 = ofClamp(t1 - increment, 0, 1);
+				t2 = ofClamp(t2 - increment, 0, 1);
 			}
 			break;
 		case OF_KEY_RIGHT:
 			// pan right
-			if (x1 == -1 && x2 == 1) {
-				t1 = ofClamp(t1 + increment, -1, 1);
-				t2 = ofClamp(t2 + increment, -1, 1);
+			if (x1 == 0 && x2 == 1) {
+				t1 = ofClamp(t1 + increment, 0, 1);
+				t2 = ofClamp(t2 + increment, 0, 1);
 			}
 			break;
 		case 'z':
 			// rotate left
-			if (t1 == -1 && t2 == 1) {
-				x1 = ofClamp(x1 - increment, -1, 1);
-				x2 = ofClamp(x2 - increment, -1, 1);
+			if (t1 == 0 && t2 == 1) {
+				x1 = ofClamp(x1 - increment, 0, 1);
+				x2 = ofClamp(x2 - increment, 0, 1);
 			}
 			break;
 		case 'x':
 			// rotate right
-			if (t1 == -1 && t2 == 1) {
-				x1 = ofClamp(x1 + increment, -1, 1);
-				x2 = ofClamp(x2 + increment, -1, 1);
+			if (t1 == 0 && t2 == 1) {
+				x1 = ofClamp(x1 + increment, 0, 1);
+				x2 = ofClamp(x2 + increment, 0, 1);
 			}
 			break;
 	}
